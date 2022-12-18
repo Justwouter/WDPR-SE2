fd5127df34dd7b574533320db6739c705c9634f2
Justwouter <wouter2312@gmail.com>
2022-12-18 13:20:27 +0100

    Added more info to the Performance class

--- backend/data/Timestamps.cs
+++ backend/data/Timestamps.cs
@@ -1,4 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+//[Owned]
 public class Timestamp{
+
     public int Id {get;set;}
     public DateTime savedDate {get;set;}
     public int? locationID {get;set;}
--- backend/model/Performance.cs
+++ backend/model/Performance.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Performance{
     public int Id {get;set;}
-    public Timestamp runtimeFrom {get;set;} = null!;
-    public Timestamp runtimeTo {get;set;} = null!;
-    public List<Timestamp> plannedShowings {get;set;} = null!;
+    public String name {get;set;} = null!;
+    public String? discription {get;set;}
+    public int runtime {get;set;} //How long a performance will take in minutes.
+    public Timestamp startDate {get;set;} = null!; //When the first performance will be performed.
+    public Timestamp endDate {get;set;} = null!; //When the last performance will be performed.
+    public List<Timestamp> plannedShowings {get;set;} = new List<Timestamp>(); //When the performance will be performed. This sorta makes start/end obsolete.
 
     //Artiesten & andere info hier!
 
