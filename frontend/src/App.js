3455457c4d4cc07e0c7c041047bdafffaae60aac
Justwouter <wouter2312@gmail.com>
2022-12-20 19:13:10 +0100

    Basic docker structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import React, {  } from 'react';
+import React, { } from 'react';
 import ProgrammaToevoegen from './components/ProgrammaToevoegen';
 import ProgrammaLijst from './components/ProgrammaLijst';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import { Route } from 'react-router';
 import './custom.css';
+import Programma from './components/Programma';
 
 const App = (props) => {
   return (
-    
-      <div className="main-content">
+
+    <div className="main-content">
       <BrowserRouter>
-       <Routes>
-        <Route element={<ProgrammaToevoegen/>} path="/ProgrammaToevoegen" />
-        <Route element={<ProgrammaLijst/>} path="/ProgrammaLijst" />
-        <Route exact path='/' element={<ProgrammaLijst/>} />
+        <Routes>
+          {/* De index element moet naar de homepage als we die hebben */}
+            <Route index element={<Programma />} /> 
+            <Route element={<ProgrammaToevoegen />} path="/ProgrammaToevoegen" />
+            <Route element={<ProgrammaLijst />} path="/ProgrammaLijst" />
+
         </Routes>
-        </BrowserRouter>
-      </div>
-   
+      </BrowserRouter>
+    </div>
+
   );
 }
 
