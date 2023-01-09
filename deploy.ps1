131b3e0ac739b9a98a08005425d750367578bd9c
Justwouter <wouter2312@gmail.com>
2023-01-09 01:26:58 +0100

    Updated the deploy script for backend support

--- deploy.ps1
+++ deploy.ps1
@@ -1,27 +1,36 @@
-$startLocation=Get-Location
-if($args.Contains("-r")){
-    Set-Location "$startLocation/frontend/src/"
-    $files = Get-Location | Get-ChildItem -Recurse | Where-Object {$_.extension -eq ".js"}
+$startLocation = Get-Location
+$folderlocations = @("$startLocation/frontend/src/", "$startLocation/backend/")
 
-    for ($i=0; $i -lt $files.Count; $i++) {
-        ((Get-Content -path $files[$i].FullName -Raw) -replace 'api.theaterlaak.site','api.localhost' -replace 'theaterlaak.site','frontend.localhost') | Set-Content -Path $files[$i].FullName -NoNewline
-    }
-    #Step two, docker
-    Set-Location $startLocation
-    ((Get-Content .\docker-compose.yml -Raw) -replace 'api.theaterlaak.site','api.localhost' -replace 'theaterlaak.site','frontend.localhost') | Set-Content .\docker-compose.yml -NoNewline
+if ($args.Contains("-r")) {
+    
+    foreach ($folder in $folderlocations) {
+        Set-Location $folder
+        $files = Get-Location | Get-ChildItem -Recurse | Where-Object { $_.extension -eq ".js" -or $_.extension -eq ".cs" }
+
+        for ($i = 0; $i -lt $files.Count; $i++) {
+            ((Get-Content -path $files[$i].FullName -Raw) -replace 'api.theaterlaak.site', 'api.localhost' -replace 'theaterlaak.site', 'frontend.localhost') | Set-Content -Path $files[$i].FullName -NoNewline
+        }
+    
         
+    }
+
+    Set-Location $startLocation
+    ((Get-Content .\docker-compose.yml -Raw) -replace 'api.theaterlaak.site', 'api.localhost' -replace 'theaterlaak.site', 'frontend.localhost') | Set-Content .\docker-compose.yml -NoNewline
+    
 
 }
 else {
-    #Step one, node
-    Set-Location "$startLocation/frontend/src/"
-    $files = Get-Location | Get-ChildItem -Recurse | Where-Object {$_.extension -eq ".js"}
 
-    for ($i=0; $i -lt $files.Count; $i++) {
-        ((Get-Content -path $files[$i].FullName -Raw) -replace 'api.localhost','api.theaterlaak.site' -replace 'frontend.localhost','theaterlaak.site') | Set-Content -Path $files[$i].FullName -NoNewline
+    foreach ($folder in $folderlocations) {
+        Set-Location $folder
+        $files = Get-Location | Get-ChildItem -Recurse | Where-Object { $_.extension -eq ".js" -or $_.extension -eq ".cs" }
+
+        for ($i = 0; $i -lt $files.Count; $i++) {
+            ((Get-Content -path $files[$i].FullName -Raw) -replace 'api.localhost', 'api.theaterlaak.site' -replace 'frontend.localhost', 'theaterlaak.site') | Set-Content -Path $files[$i].FullName -NoNewline
+        }
     }
-    #Step two, docker
+
     Set-Location $startLocation
-    ((Get-Content .\docker-compose.yml -Raw) -replace 'api.localhost','api.theaterlaak.site' -replace 'frontend.localhost','theaterlaak.site') | Set-Content .\docker-compose.yml -NoNewline
+    ((Get-Content .\docker-compose.yml -Raw) -replace 'api.localhost', 'api.theaterlaak.site' -replace 'frontend.localhost', 'theaterlaak.site') | Set-Content .\docker-compose.yml -NoNewline
         
 }
