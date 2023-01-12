d8f3d5e08c0671ad9b4bfb432ed43a69300b45bc
Justwouter <wouter2312@gmail.com>
2023-01-12 21:34:09 +0100

    Removed the https capability from back/frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -76,7 +76,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthentication();
