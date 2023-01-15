0515ec0b24c367a354a5a259f7c2ec8086954098
Justwouter <wouter2312@gmail.com>
2023-01-15 18:21:54 +0100

    Better Bash deploy

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,47 @@
 #!/usr/bin/env bash
+#Yes the urls could also be changed by the prod/dev compose files but where is the fun in that?
+ReplaceInFile() {
+    for condition in $2; do
+        sed -i "$condition" "$1"
+    done
+    sed -i "$lineEnding" "$1"
 
+}
+
+deploy() {
+    if [[ "$1" == *"-r"* ]]; then
+        #When argument -r is passed, restore urls to their localhost equivalent.
+        for folder in "${folderlocations[@]}"; do
+            cd "$folder" || exit
+            files=$(find . -name '*.js' -o -name '*.cs')
+            for file in $files; do
+                ReplaceInFile "$file" "$DevSedConditions"
+            done
+        done
+        #Add a seperate check for docker-compose, this is easier than excluding all script files in the root dir.
+        cd "$startLocation" || exit
+        ReplaceInFile docker-compose.yml "$DevSedConditions"
+    else
+        #Check if a file contains a specific url & change it to its production value
+        for folder in "${folderlocations[@]}"; do
+            cd "$folder" || exit
+            files=$(find . -name '*.js' -o -name '*.cs')
+            for file in $files; do
+                ReplaceInFile "$file" "$ProdSedConditions"
+            done
+        done
+        #Add a seperate check for docker-compose, this is easier than excluding all script files in the root dir.
+        cd "$startLocation" || exit
+        ReplaceInFile docker-compose.yml "$ProdSedConditions"
+    fi
+}
+
+#Global vars
 startLocation=$(pwd)
 folderlocations=("$startLocation/frontend/src/" "$startLocation/backend/")
-
 CR=$(printf '\r')
+DevSedConditions=('s/api.theaterlaak.site/api.localhost/g' 's/theaterlaak.site/frontend.localhost/g')
+ProdSedConditions=('s/api.localhost/api.theaterlaak.site/g' 's/frontend.localhost/theaterlaak.site/g')
+lineEnding="$ ! s/\$/$CR/" #Inserts a carriage return after every line ending except the last otherwise newlines will be appended to a file everytime this condition is used.
 
-
-if [[ "$1" == *"-r"* ]]; then
-    #When argument -r is passed, restore urls to their localhost equivalent.
-    for folder in "${folderlocations[@]}"; do
-        cd "$folder" || exit
-        files=$(find . -name '*.js' -o -name '*.cs')
-
-        for file in $files; do
-            sed -i 's/api.theaterlaak.site/api.localhost/g' "$file"
-            sed -i 's/theaterlaak.site/frontend.localhost/g' "$file"
-            sed -i "$ ! s/\$/$CR/" "$file"
-        done
-
-    done
-
-    #Add a seperate check for docker-compose, this is easier than excluding all script files in the root dir.
-    cd "$startLocation" || exit
-    sed -i 's/api.theaterlaak.site/api.localhost/g' docker-compose.yml
-    sed -i 's/theaterlaak.site/frontend.localhost/g' docker-compose.yml
-    sed -i "$ ! s/\$/$CR/" docker-compose.yml
-
-else
-    #Check if a file contains a specific url & change it to its production value
-    for folder in "${folderlocations[@]}"; do
-        cd "$folder" || exit
-        files=$(find . -name '*.js' -o -name '*.cs')
-
-        for file in $files; do
-            sed -i 's/api.localhost/api.theaterlaak.site/g' "$file"
-            sed -i 's/frontend.localhost/theaterlaak.site/g' "$file"
-            sed -i "$ ! s/\$/$CR/" "$file"
-
-        done
-
-    done
-
-    #Add a seperate check for docker-compose, this is easier than excluding all script files in the root dir.
-    cd "$startLocation" || exit
-    sed -i 's/api.localhost/api.theaterlaak.site/g' docker-compose.yml
-    sed -i 's/frontend.localhost/theaterlaak.site/g' docker-compose.yml
-    sed -i "$ ! s/\$/$CR/" docker-compose.yml
-
-fi
+deploy "$@"
