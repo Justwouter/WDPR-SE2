3a5b1970abcfb442d43950226e440fa536c5ef04
Justwouter <wouter2312@gmail.com>
2023-01-15 02:25:53 +0100

    Added compose argument support to Run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 runfunc() {
     if [[ "$1" == *"-dcd"* ]]; then
         ./deploy.sh
-        docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+        docker-compose -f docker-compose.yml -f docker-compose.prod.yml up "${@:2}"
         return
     elif [[ "$1" == *"-dc"* ]]; then
         ./deploy.sh -r
-        docker-compose -f docker-compose.yml -f docker-compose.test.yml up
+        docker-compose -f docker-compose.yml -f docker-compose.test.yml up "${@:2}"
         return
     fi
 
