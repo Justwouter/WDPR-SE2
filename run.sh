7874fabfc4da94201eb12572ed4000f878d68b7a
Justwouter <wouter2312@gmail.com>
2023-01-15 17:31:04 +0100

    Added text aliases to run

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 runfunc() {
-    if [[ "$1" == *"-dcd"* ]]; then
+    if [[ "$1" == *"-dcd"* || "$1" == *"prod"* ]]; then
         ./deploy.sh
         docker-compose -f docker-compose.yml -f docker-compose.prod.yml up "${@:2}"
         return
-    elif [[ "$1" == *"-dc"* ]]; then
+    elif [[ "$1" == *"-dc"* || "$1" == *"dev"* ]]; then
         ./deploy.sh -r
         docker-compose -f docker-compose.yml -f docker-compose.test.yml up "${@:2}"
         return
