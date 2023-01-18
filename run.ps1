c8ab9dfd2f0de29d4dac0ccca0867d40c629ac3a
Justwouter <wouter2312@gmail.com>
2023-01-18 15:26:30 +0100

    Update only dbup, pass args to run, Change test dbs

--- run.ps1
+++ run.ps1
@@ -1,13 +1,17 @@
 $startLocation=Get-Location
+
 if ($args.contains("-dcd")) {
     ./deploy
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml up $args[1..($args.Count-1)]
     continue
 }
 elseif ($args.contains("-dc")) {
     ./deploy -r
-    docker-compose -f docker-compose.yml -f docker-compose.test.yml up
+    docker-compose -f docker-compose.yml -f docker-compose.test.yml up $args[1..($args.Count-1)]
     continue
+}
+elseif ($args.Contains("-t")) {
+    
 }
 else {
     Set-Location "$startLocation/frontend"
