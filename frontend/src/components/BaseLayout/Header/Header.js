a6498284fee77b2b231036074d309b628f5ff935
Justwouter <wouter2312@gmail.com>
2023-01-19 19:54:40 +0100

    Working 2 step auth for admin menu

--- frontend/src/components/BaseLayout/Header/Header.js
+++ frontend/src/components/BaseLayout/Header/Header.js
@@ -2,31 +2,33 @@ import React, { useEffect, useState } from 'react'
 import HeadItem from './HeadItem'
 import { useNavigate } from 'react-router-dom'
 
-function Header () {
+function Header() {
+  //First check the JWT token for a role then verify with the API. Only JWT is insecure but the api takes to long.
   const [AdminComponents, setAdminComponents] = useState(false)
-  // useEffect(() => {
-  //   async function fetchData () {
-  //     const jwtToken = localStorage.getItem('jwt').replace('"', '')
-  //     console.log(jwtToken)
-  //     fetch('http://api.localhost/api/Role/CheckElevation', {
-  //       method: 'GET',
-  //       headers: {
-  //         Accept: 'text/plain',
-  //         'Content-Type': 'text/plain',
-  //         Authorization: 'Bearer ' + jwtToken
-  //       }
-  //     }).then(response => {
-  //       if (response.status === 200) {
-  //         setAdminComponents(true)
-  //       } else {
-  //         setAdminComponents(false)
-  //       }
-  //     })
-  //   }
-  //   fetchData()
-  // }, [])
   useEffect(() => {
-    function parseJwt (token) {
+    async function fetchData () {
+      const jwtToken = localStorage.getItem('jwt').replace('"', '')
+      console.log(jwtToken)
+      fetch('http://api.localhost/api/Role/CheckElevation', {
+        method: 'GET',
+        headers: {
+          Accept: 'text/plain',
+          'Content-Type': 'text/plain',
+          Authorization: 'Bearer ' + jwtToken
+        }
+      }).then(response => {
+        if (response.status === 200) {
+          setAdminComponents(true)
+        } else {
+          setAdminComponents(false)
+        }
+      })
+    }
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    function parseJwt(token) {
       var base64Url = token.split('.')[1]
       var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
       var jsonPayload = decodeURIComponent(
@@ -41,7 +43,13 @@ function Header () {
 
       return JSON.parse(jsonPayload)
     }
-    
+    const jwtToken = parseJwt(localStorage.getItem('jwt'))
+    if (jwtToken.role === "Medewerker" | jwtToken.role === "Admin") {
+      setAdminComponents(true)
+    }
+    else {
+      setAdminComponents(false)
+    }
   }, [])
 
   let navigate = useNavigate()
