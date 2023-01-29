8f410779945a19f308352bbcd03a0e0cc0613729
Justwouter <wouter2312@gmail.com>
2023-01-29 18:54:25 +0100

    Added support for optional custom classes to HeadItem

--- frontend/src/components/BaseLayout/Header/HeadItem.js
+++ frontend/src/components/BaseLayout/Header/HeadItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HeadItem(headeritem) {
   return (
-    <li className="list-inline-item"><a href={headeritem.link}>{headeritem.text}</a></li>
+    <li className={headeritem.className != null?headeritem.className:"list-inline-item"}><a href={headeritem.link}>{headeritem.text}</a></li>
   );
 }
 
